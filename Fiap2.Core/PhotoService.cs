086a1f35ef38c9336674970eb9438eaa9e9b7359
Rodolfo Fadino <guitarlefthand@hotmail.com>
2017-09-08 00:35:33 -0300

    use configuration for api url

--- Fiap2.Core/PhotoService.cs
+++ Fiap2.Core/PhotoService.cs
@@ -6,21 +6,30 @@ using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace Fiap2.Core
 {
     public class PhotoService : IPhotoService
     {
+        private readonly IConfiguration _configuration;
+
+        public PhotoService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public List<Photo> List()
         {
             var client = new HttpClient();
+            var apiUrl = _configuration.GetSection("AppSettings").GetValue<string>("PhotoApiUrl");
+            var stringTask = client.GetStringAsync(apiUrl);
+            var response = string.Empty;
 
-            var stringTask = client.GetStringAsync("https://jsonplaceholder.typicode.com/photos");
-            var response = "";
             Task.Run(async () =>
             {
                 response = await stringTask as string;
             }).Wait();
+
             var photoList = JsonConvert.DeserializeObject<List<Photo>>(response);
 
             return photoList.Take(20).ToList();
