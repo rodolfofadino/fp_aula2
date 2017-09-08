e99209987bafebb3ea22c6e9097c8547c0f723b2
Rodolfo Fadino <guitarlefthand@hotmail.com>
2017-09-08 09:54:32 -0300

    implement cache on photoservice

--- Fiap2.Core/PhotoService.cs
+++ Fiap2.Core/PhotoService.cs
@@ -7,30 +7,42 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Fiap2.Core
 {
     public class PhotoService : IPhotoService
     {
         private readonly IConfiguration _configuration;
+        private IMemoryCache _cache;
 
-        public PhotoService(IConfiguration configuration)
+
+        public PhotoService(IConfiguration configuration, IMemoryCache cache)
         {
+            _cache = cache;
             _configuration = configuration;
         }
         public List<Photo> List()
         {
-            var client = new HttpClient();
-            var apiUrl = _configuration.GetSection("AppSettings").GetValue<string>("PhotoApiUrl");
-            var stringTask = client.GetStringAsync(apiUrl);
-            var response = string.Empty;
-
-            Task.Run(async () =>
+            const string cacheKey = "photoList";
+            List<Photo> photoList;
+            if (!_cache.TryGetValue(cacheKey, out photoList))
             {
-                response = await stringTask as string;
-            }).Wait();
+                var client = new HttpClient();
+                var apiUrl = _configuration.GetSection("AppSettings").GetValue<string>("PhotoApiUrl");
+                var stringTask = client.GetStringAsync(apiUrl);
+                var response = string.Empty;
 
-            var photoList = JsonConvert.DeserializeObject<List<Photo>>(response);
+                Task.Run(async () =>
+                {
+                    response = await stringTask as string;
+                }).Wait();
+
+                photoList = JsonConvert.DeserializeObject<List<Photo>>(response);
+
+                var cacheTime = _configuration.GetSection("AppSettings").GetValue<int>("CacheTime");
+                _cache.Set(cacheKey, photoList, DateTimeOffset.Now.AddSeconds(cacheTime));
+            }
 
             return photoList.Take(20).ToList();
         }
--- Fiap2.Web/Startup.cs
+++ Fiap2.Web/Startup.cs
@@ -22,10 +22,11 @@ namespace Fiap2.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-        //    services.AddScoped<Core.IPhotoService, PhotoService>();
-        //    services.AddSingleton<Core.IPhotoService, PhotoService>();
+            //    services.AddScoped<Core.IPhotoService, PhotoService>();
+            //    services.AddSingleton<Core.IPhotoService, PhotoService>();
             services.AddTransient<Core.IPhotoService, PhotoService>();
             services.AddMvc();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,7 @@ namespace Fiap2.Web
             }
 
             app.UseStaticFiles();
-
+            
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
